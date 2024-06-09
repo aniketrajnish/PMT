522f12514ae3db41747a827b517a28fb79387712
Aniket Rajnish <58925008+aniketrajnish@users.noreply.github.com>
2024-06-08 22:25:57 -0400

    [Add] Path details

--- src/PMT/PMT/paths.py
+++ src/PMT/PMT/paths.py
@@ -3,9 +3,9 @@ import os
 class Paths:
     paths = {
         'parent' : os.path.join(os.getenv('LOCALAPPDATA'), 'PMT'),
-        'unity' : r'placeholder',
-        'maya' : r'placeholder',
-        'houdini' : r'placeholder',
+        'unreal' : r'C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32\EpicGamesLauncher.exe',
+        'maya' : r'C:\Program Files\Autodesk\Maya2024\bin\maya.exe',
+        'houdini' : r'C:\Program Files\Side Effects Software\Houdini 19.5.640\bin\houdinifx.exe',
         }
     
     @staticmethod
